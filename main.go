208b9704bc6addfe32cb6a1cb4ec77f6f3d4fa4d
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2024-09-27 12:35:57 +0200

    feat: add timestamp to response and refactor main.go

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte("OK"))
+		_, err := w.Write([]byte(getStringWithTimestamp()))
 		if err != nil {
 			return
 		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil)) //nolint:gosec
+}
+
+// getStringWithTimestamp returns the string with "OK" and the current timestamp
+func getStringWithTimestamp() string {
+	return fmt.Sprintf("OK - %s", time.Now().Format(time.RFC3339))
 }
